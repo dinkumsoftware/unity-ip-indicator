1fc88ed7ff3e173bf19498f91f06ba4f2b11e0ad
tom campbell <tc@DinkumSoftware.com>
2016-05-08 08:24:16 -0400

    Display multiple IP addrs on machines with multiple interfaces

--- ip.py
+++ ip.py
@@ -5,15 +5,32 @@ import appindicator
 import gtk
 
 # 2016-05-07 tc@DinkumSoftware.com Bug fix.  Suppress localhost 127.0.0.1
+# 2016-05-08 tc@DinkumSoftware.com Bug fix.  Display multiple IP numbers
 
 ICON = os.path.abspath("./images/icon.png")
 
 def get_ip():
+
+    # The first grep produces
+    #     inet addr:127.0.0.1
+    #     inet addr:10.23.4.15
+    #     inet addr:172.27.224.230
+    # The 2nd grep removes the 127 addr (localhost)
+    # The 3rd grep produces:
+    #     172.27.224.230
+    #     10.23.4.15
+
     ip = subprocess.check_output('ifconfig |\
         grep -o -P "inet addr:([^ ]*)"     |\
         grep -v    "127.0.0.1"             |\
-        grep -o -m 1 -P "[0-9.]+"', shell=True)
-    return ip.strip()
+        grep -o -P "[0-9.]+"',     shell=True)
+
+    # turn multiple lines of IP addresses into
+    # a single line with IPs space separated
+    ip  = ip.strip()
+    ipl = ip.split('\n')  # The list
+    ip  = ' '.join(ipl)
+    return ip
 
 
 class IPIndicator:
